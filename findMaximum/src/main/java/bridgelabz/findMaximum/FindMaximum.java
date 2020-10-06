49ef6aa95e8815c136f2f2f734528ff82324acda
Shubham663 <mittalshubham05@gmail.com>
2020-10-06 20:53:32 +0530

    printing maximum element through generic function

--- findMaximum/src/main/java/bridgelabz/findMaximum/FindMaximum.java
+++ findMaximum/src/main/java/bridgelabz/findMaximum/FindMaximum.java
@@ -1,10 +1,7 @@
 package bridgelabz.findMaximum;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
 
 /**
  * Hello world!
@@ -45,43 +42,13 @@ public class FindMaximum<T extends Comparable<T>>{
 	public void setT3(T t3) {
 		this.t3 = t3;
 	}
-	/**
-	 * @return, returns the maximum of the three arguments
-	 */
-	public static Integer getMaxInteger(Integer a, Integer b, Integer c) {
-		return Integer.max(c,Integer.max(a, b));
-	}
 	
     public static void main( String[] args )
     {
-    	Scanner sc = new Scanner(System.in);
     	System.out.println("Getting the maximum of numbers"
     			+ "\n 1,4,65,34,76,59,23,17");
-        System.out.println("The maximum of the multiple values is : " + getMax(1,4,65,34,76,59,23,17));
+    	getMax(1,4,65,34,76,59,23,17);
     }
-
-	/**
-	 * @return, returns the maximum of the strings provided as arguments
-	 */
-	public static String getMaxString(String a, String b, String c) {
-		return a.compareTo(b) > 0 ? (a.compareTo(c) > 0 ? a : c):(b.compareTo(c) > 0 ? b : c );  
-	}
-
-	/**
-	 * @return, returns the maximum of the three float values
-	 */
-	public static Float getMaxFloat(final Float i,final Float j,final Float k) {
-		return Float.max(i, Float.max(j, k));
-	}	
-
-	/**
-	 * @param <T>, the generic type
-	 * @return, returns the maximum of the three elements of T type
-	 */
-	public static <T extends Comparable<T>> T getMax(T t1,T t2, T t3) {
-		return t1.compareTo(t2) > 0 ? (t1.compareTo(t3) > 0 ? t1 : t3):(t2.compareTo(t3) > 0 ? t2 : t3 );
-	}
-
 	/**
 	 * @param <T>, the generic type
 	 * @param tArray, the values from which maximum is to be determined
@@ -90,9 +57,13 @@ public class FindMaximum<T extends Comparable<T>>{
 	public static <T extends Comparable<T>> T getMax(T... tArray) {
 		List<T> tList = Arrays.asList(tArray);
 		Collections.sort(tList);
+		printMax(tList.get(tList.size()-1));
 		return tList.get(tList.size()-1);
 	}
 	
+	private static<T> void printMax(T t) {
+		System.out.println("The maximum value printed from printMax : " + t);
+	}
 	public int compareTo(T o) {
 		return this.compareTo(o);
 	}
